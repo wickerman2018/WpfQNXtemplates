808270e5350c8c0fc4f6e7ac689b4b7f3a76c68f
altukhov <is1ntc@gmail.com>
2019-10-02 12:52:15 +0300

    Добавлена генерация кода создания потока

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,54 @@ namespace WpfQNXtemplates
         {
             InitializeComponent();
         }
+
+        private void BtnThreadGen_Click(object sender, RoutedEventArgs e)
+        {
+            String ThreadDetachAttr= "PTHREAD_CREATE_DETACHED";
+            String ThreadSchedpolicy = "SCHED_NOCHANGE";
+            switch (cbThreadDetachAttr.SelectedIndex)
+            {
+                case 0: 
+                    ThreadDetachAttr = "PTHREAD_CREATE_JOINABLE"; 
+                    break;
+                case 1:
+                    ThreadDetachAttr = "PTHREAD_CREATE_DETACHED";
+                    break;
+            }
+            switch (cbThreadSchedpolicy.SelectedIndex)
+            {
+                case 0:
+                    ThreadSchedpolicy = "SCHED_FIFO";
+                    break;
+                case 1:
+                    ThreadSchedpolicy = "SCHED_NOCHANGE";
+                    break;
+                case 2:
+                    ThreadSchedpolicy = "SCHED_RR";
+                    break;
+                case 3:
+                    ThreadSchedpolicy = "SCHED_SPORADIC";
+                    break;
+            }
+
+            tbSourceGen.Clear();
+            //Создание функции
+            tbSourceGen.AppendText("static void *" + tbThreadName.Text + " (void *){" + Environment.NewLine
+                + Environment.NewLine +
+                "return 0; " + Environment.NewLine +
+                "};" + Environment.NewLine + Environment.NewLine);
+            //Запуск потока
+            tbSourceGen.AppendText("pthread_attr_t attr_th;"+Environment.NewLine);
+            tbSourceGen.AppendText("pthread_attr_init(&attr_th);" + Environment.NewLine);
+            tbSourceGen.AppendText("pthread_attr_setdetachstate(&attr_th, "+ ThreadDetachAttr + ");" + Environment.NewLine);
+            if (cbThreadSchedpolicy.SelectedIndex != 1)
+                tbSourceGen.AppendText("pthread_attr_setschedpolicy(&attr_th, " + ThreadSchedpolicy + ");" + Environment.NewLine);
+            tbSourceGen.AppendText("pthread_create(NULL, &attr_th, " + tbThreadName.Text + ", NULL);" + Environment.NewLine);
+        }
+
+        private void BtnCopyToBuf_Click(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(tbSourceGen.Text);
+        }
     }
 }
